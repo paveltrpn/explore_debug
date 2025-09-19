1a389a90cd109f3017eaf7e4f98891d4e5dd19c4
Pavel Turapin <paveltrpn@integra-s.com>
2025-09-19 11:56:28 +0400

    add test code

--- src/script.js
+++ src/script.js
@@ -1,6 +1,100 @@
 import { Range } from "./range.js";
 import * as smnp from "./string_mnp.js";
 
+let duration = "";
+
+const formatDurationMessage = (diff, durationType) => {
+    let min = "";
+    let hrs = "";
+    let dys = "";
+    let wks = "";
+    let mts = "";
+    let yrs = "";
+
+    if (11 <= diff % 100 && diff % 100 <= 14) {
+        min = "минут";
+        hrs = "часов";
+        dys = "суток";
+        wks = "недель";
+        mts = "месяцев";
+        yrs = "лет";
+    } else if (diff % 10 == 1) {
+        min = "минуту";
+        hrs = "час";
+        dys = "сутки";
+        wks = "неделю";
+        mts = "месяц";
+        yrs = "год";
+    } else if (2 <= diff % 10 && diff % 10 <= 4) {
+        min = "минуты";
+        hrs = "часа";
+        dys = "суток";
+        wks = "недели";
+        mts = "месяца";
+        yrs = "лет";
+    } else {
+        min = "минут";
+        hrs = "часов";
+        dys = "суток";
+        wks = "недель";
+        mts = "месяцев";
+        yrs = "лет";
+    }
+
+    let form = "";
+    switch (durationType) {
+        case "min": {
+            form = min;
+            break;
+        }
+        case "hrs": {
+            form = hrs;
+            break;
+        }
+        case "dys": {
+            form = dys;
+            break;
+        }
+        case "wks": {
+            form = wks;
+            break;
+        }
+        case "mts": {
+            form = mts;
+            break;
+        }
+        case "yrs": {
+            form = yrs;
+            break;
+        }
+        default: {
+            break;
+        }
+    }
+
+    return `Обновлено ${diff} ${form} назад`;
+};
+
+const diff = 60 * 60 * 24 * 28 * 12;
+
+if (diff < 60) {
+    duration = "Обновлено меньше 1 минуты назад";
+} else if (diff < 60 * 60) {
+    duration = formatDurationMessage(Math.trunc(diff / 60), "min");
+} else if (diff < 60 * 60 * 24) {
+    duration = formatDurationMessage(Math.trunc(diff / (60 * 60)), "hrs");
+} else if (diff < 60 * 60 * 24 * 7) {
+    duration = formatDurationMessage(Math.trunc(diff / (60 * 60 * 24)), "dys");
+} else if (diff < 60 * 60 * 24 * 28) {
+    duration = formatDurationMessage(Math.trunc(diff / (60 * 60 * 24 * 7)), "wks");
+} else if (diff < 60 * 60 * 24 * 28 * 12) {
+    duration = formatDurationMessage(Math.trunc(diff / (60 * 60 * 24 * 28)), "mts");
+} else {
+    duration = formatDurationMessage(Math.trunc(diff / (60 * 60 * 24 * 28 * 12)), "yrs");
+}
+
+console.log(`diff is: ${duration}`);
+
 function timeout(delay) {
     const timeoutPromise = new Promise(() => {
         setTimeout(() => {
